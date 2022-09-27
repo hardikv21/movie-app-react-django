d40d8ef92778178588bef7cd56916b591fdc4928
Hardik Vora <hardik7304@gmail.com>
2022-09-26 20:52:20 -0400

    Backend search functionality added

--- backend/movie/views.py
+++ backend/movie/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 
 from movie.models import Movie
@@ -8,3 +8,5 @@ from movie.serializers import MovieSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
