6e834407b2a6295f84c016fefd7c49aad4b03507
Hardik Vora <hardik7304@gmail.com>
2022-09-28 16:51:30 -0400

    Pagination added in the backend

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    releaseYear = models.PositiveSmallIntegerField(blank=True, null=True)
+    releaseYear = models.CharField(max_length=100)
     poster = models.ImageField(upload_to='images/')
     created = models.DateTimeField(auto_now_add=True)
     like = models.IntegerField(default=0)
--- backend/movie/views.py
+++ backend/movie/views.py
@@ -1,12 +1,37 @@
 from rest_framework import viewsets, filters
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+import requests
 
 from movie.models import Movie
 from movie.serializers import MovieSerializer
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    # page_size_query_param = 'page_size'
+    # max_page_size = 1000
+
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = StandardResultsSetPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
+
+    # def list(self, request):
+    #     header = {
+    #         "Content-Type":"application/json",
+    #         "X-Client-Id":None,
+    #         "X-Client-Secret":None,
+    #     }
+    #     response = requests.get('http://www.omdbapi.com?apikey=f0e7f017&s=koi&page=1', headers=header)
+    #     response = response.json()
+    #     for item in response["Search"]:
+    #         print(item["Title"])
+    #         Movie.objects.create(title=item["Title"], releaseYear=item["Year"], poster=item["Poster"])
+            
+    #     movies = Movie.objects.all()
+    #     serializer = MovieSerializer(movies, many=True)
+    #     return Response(serializer.data)
