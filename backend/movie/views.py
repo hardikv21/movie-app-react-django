80e1abf6b9ff1e6ef29d02d526f0d01a1e4a62e6
Hardik Vora <hardik7304@gmail.com>
2022-09-28 16:52:07 -0400

    Non used code removed

--- backend/movie/views.py
+++ backend/movie/views.py
@@ -9,8 +9,6 @@ from movie.serializers import MovieSerializer
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
-    # page_size_query_param = 'page_size'
-    # max_page_size = 1000
 
 
 class MovieViewSet(viewsets.ModelViewSet):
