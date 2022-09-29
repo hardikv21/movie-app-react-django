f3425e297567c362ba8338b17c1d3b5c2bc7bbae
Hardik Vora <hardik7304@gmail.com>
2022-09-29 16:09:30 -0400

    Image upload part updated in backend

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 
+def upload_to(instance, filename):
+    return 'images/{filename}'.format(filename=filename)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     releaseYear = models.CharField(max_length=100)
-    poster = models.ImageField(upload_to='images/')
+    poster = models.ImageField(upload_to=upload_to, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     like = models.IntegerField(default=0)
     dislike = models.IntegerField(default=0)
--- backend/movie/serializers.py
+++ backend/movie/serializers.py
@@ -3,6 +3,8 @@ from movie.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    poster = serializers.ImageField(required=False)
+    
     class Meta:
         model = Movie
         fields = '__all__'
