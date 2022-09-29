ead1952ad1a26ff7901f092b1f1fca977830f14b
Hardik Vora <hardik7304@gmail.com>
2022-09-29 15:44:59 -0400

    Non used code removed

--- backend/movie/views.py
+++ backend/movie/views.py
@@ -19,30 +19,3 @@ class MovieViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
     parser_classes = (MultiPartParser, FormParser)
-
-    def post(self, request, *args, **kwargs):
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk, *args, **kwargs):
-        todo_instance = self.get_object()
-        if not todo_instance:
-            return Response(
-                {"res": "Object with todo id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        data = {
-            'title': request.data.get('title'), 
-            'releaseYear': request.data.get('releaseYear'),
-            # 'poster': request.data.get('poster'),
-            'like': request.data.get('like'), 
-            'dislike': request.data.get('dislike')
-        }
-        serializer = MovieSerializer(instance = todo_instance, data=data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
